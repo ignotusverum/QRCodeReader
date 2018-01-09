a14630361946752aa01af890dfe8895ca27b9c38
ignotusverum <vladislavz1991@gmail.com>
2018-01-09 17:58:24 -0500

    action buttons

--- QRCodeReader/Secions/Camera/CameraViewController.swift
+++ QRCodeReader/Secions/Camera/CameraViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 import Foundation
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    /// QR session capture
+    var captureSession: AVCaptureSession?
+    
+    /// Camera roll button
+    lazy var rightButton: UIBarButtonItem = {
+       
+        let button = UIBarButtonItem(image: #imageLiteral(resourceName: "camera-roll"), style: .plain, target: self, action: #selector(onRightButton(_:)))
+        button.tintColor = .black
+        return button
+    }()
+    
+    /// Flashlight button
+    lazy var leftButton: UIBarButtonItem = {
+    
+        let button = UIBarButtonItem(image: #imageLiteral(resourceName: "flash"), style: .plain, target: self, action: #selector(onLeftButton(_:)))
+        button.tintColor = .black
+        return button
+    }()
     
     // MARK: Controller lifecycle
     override func viewDidLoad() {
@@ -19,7 +39,22 @@ class CameraViewController: UIViewController {
     }
     
     private func layoutSetup() {
+     
+        navigationItem.leftBarButtonItem = leftButton
+        navigationItem.rightBarButtonItem = rightButton
         
         setNavigationImage(#imageLiteral(resourceName: "logo"))
+        view.backgroundColor = .white
+    }
+    
+    // MARK: Utilities
+    @objc
+    private func onRightButton(_ sender: Any?) {
+        
+    }
+    
+    @objc
+    private func onLeftButton(_ sender: Any?) {
+        
     }
 }
